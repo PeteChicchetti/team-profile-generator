7538fca90d87204ba0ef9a832348c90e41334452
PeteChicchetti <pj0524@gmail.com>
2022-10-25 20:38:30 -0400

    employee.test passed

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,14 +1,34 @@
 const Employee = require('../lib/Employee.classes');
+const mockEmployee = new Employee ('Pete', '01', 'petechicchetti@gmail.com');
 
 describe('Employee', () => {
     describe('init', () => {
         it('should create an employee with name, id and email', () => {
-            const employee = new Employee('Pete', 1, 'petechicchetti@gmail.com');
-            expect(employee.name).toBe(true);
-            expect(employee.id).toBe(true);
-            expect(employee.email).toBe(true);
-        })
-    })
-
-
-})
+            expect(mockEmployee.name).toBe('Pete');
+            expect(mockEmployee.id).toBe('01')
+            expect(mockEmployee.email).toBe('petechicchetti@gmail.com')
+        });
+    });
+    describe('getName', () => {
+        it('should return the value of the name of the new employee', () => {
+            expect(mockEmployee.getName()).toBe('Pete');
+        });
+    });
+    describe('getId', () => {
+        it('should return the value of the id of the new employee', () => {
+            expect(mockEmployee.getId()).toBe('01');
+        });
+    });
+    describe('getEmail', () => {
+        it('should return the value of the email of the new employee', () => {
+            expect(mockEmployee.getEmail()).toBe('petechicchetti@gmail.com');
+        });
+    });
+    describe('getRole', () => {
+        it('should return the value assigned to the given role', () => {
+            const role = 'Employee';
+            const newRole = new Employee().getRole();
+            expect(role).toBe(newRole);
+        });
+    });
+});
